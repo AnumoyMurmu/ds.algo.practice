2d7f9f452ff7bd3e369aeccaff0828de56472d1b
Anumoy Murmu <103399542+AnumoyMurmu@users.noreply.github.com>
2023-08-21 23:04:28 +0530

    initial commit

---
+++ unordered_map.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+      int n;
+  cout<<"Enter the size of n:";
+  cin>>n;
+  int arr[n];
+  cout<<"Enter the array elements:";
+
+  for(int i=0; i<n; i++)
+  {
+    cin>>arr[i];
+  } 
+
+  //pre-computation
+  unordered_map<int, int>mpp;
+  for(int i=0; i<n; i++)
+  {
+    mpp[arr[i]]++;
+  }
+
+  for(auto i:mpp)
+  {
+
+    cout<<i.first<<endl;
+  }
+
+
+//query
+  int q;
+  cout<<"Enter the size of q:";
+  cin>>q;
+ while(q--)
+  {
+
+    int number;
+    cout<<"Enter the number u want to do a query about";
+
+    cin>>number;
+
+    // fetching
+
+    cout<<mpp[number]<<endl;
+  }
+
+    return 0;
+}
